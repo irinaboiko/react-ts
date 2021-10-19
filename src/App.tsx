06c31647ead5c16878dc5a6ef69e2ee3318e8c42
Irina Boiko <irinaboiko95@gmail.com>
2021-10-19 15:58:32 +0300

    typed Component Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import CardPageContainer, {
+  CardVariant,
+} from "./pages/CardPage/containers/CartPageContainer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CardPageContainer
+      width="200px"
+      height="200px"
+      variant={CardVariant.outlined}
+      onClick={(num) => console.log("clicked!!!", num)}
+    >
+      <button>click me</button>
+    </CardPageContainer>
   );
 }
 
